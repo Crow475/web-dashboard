80e5f20ac3fe6ec2d8f9e783c12d6fb9b4cc96ca
Crow475 <spicable475@gmail.com>
2026-05-09 23:45:06 +0400

    Try fix session fetching 2

--- proxy.ts
+++ proxy.ts
@@ -2,23 +2,35 @@ import { NextRequest, NextResponse } from "next/server";
 import { headers } from "next/headers";
 import { auth } from "@/lib/auth";
 
-export async function proxy(request: NextRequest) {
-    const session = await auth.api.getSession({
-        headers: await headers(),
-    });
+import { getSessionCookie } from "better-auth/cookies";
 
-    if (!session) {
+export async function proxy(request: NextRequest) {
+    // const session = await auth.api.getSession({
+    //     headers: await headers(),
+    // });
+
+    // if (!session) {
+    //     return NextResponse.redirect(new URL("/sign-in", request.url));
+    // }
+
+    // console.log("Session found in proxy middleware:", {
+    //     user: {
+    //         email: session.user.email,
+    //         name: session.user.name,
+    //         id: session.user.id,
+    //         emailVerified: session.user.emailVerified,
+    //     },
+    // });
+
+    const sessionCookie = getSessionCookie(request);
+    const { pathname } = request.nextUrl;
+
+    if (sessionCookie && ["/sign-in", "/sign-up"].includes(pathname)) {
+        return NextResponse.redirect(new URL("/app", request.url));
+    }
+    if (!sessionCookie && pathname.startsWith("/app")) {
         return NextResponse.redirect(new URL("/sign-in", request.url));
     }
-
-    console.log("Session found in proxy middleware:", {
-        user: {
-            email: session.user.email,
-            name: session.user.name,
-            id: session.user.id,
-            emailVerified: session.user.emailVerified,
-        },
-    });
 
     // if (session.user.emailVerified === false) {
     //     return NextResponse.redirect(new URL(`/verify-email?email=${session.user.email}`, request.url));
